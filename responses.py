6df9c25a60bf9672c0dc1b6d558bb9a0ea9a8095
Fidelio Ciandy <fiocheah08@gmail.com>
2024-02-07 22:03:01 -0600

    make dictionary feature

--- responses.py
+++ responses.py
@@ -1,4 +1,5 @@
 import random
+import requests
 from discord.ext import commands
 
 
@@ -13,6 +14,32 @@ class Responses(commands.Cog):
     @commands.command()
     async def roll(self, ctx):
         await ctx.send(str(random.randint(1, 6)))
+
+    @commands.command()
+    async def definition(self, ctx, word):
+        base_url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
+        try:
+            # request to WordsAPI
+            response = requests.get(f"{base_url}")
+            data = response.json()
+            print(data)
+            # Extract and concatenate all definitions into a string
+            definitions_string = ""
+            index = 1
+            for meaning in data[0]["meanings"]:
+                for definition_info in meaning["definitions"]:
+                    definition = definition_info["definition"]
+                    definitions_string += f"Definition {index}: {definition}\n\n"
+                    index += 1
+
+            # # Print the concatenated string
+            # print(definitions_string)
+
+            await ctx.send(definitions_string)
+
+        except Exception as e:
+            print(f"Failed to retrieve meaning: {e}")
+            await ctx.send("Failed to retrieve the meaning. Please try again later.")
 
 
 def setup(bot):
